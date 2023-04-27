b4ba6986e749874a2afb63c03526b4952d1f068f
shakhbos <khamrayevsh12@gmail.com>
2023-04-28 04:04:28 +0500

    First commit

---
+++ src/main/java/com/example/myprojects/MyProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.myprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myprojects/MyProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
